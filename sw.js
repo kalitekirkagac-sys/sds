399b36df028386b2d66dc871cebb864643665569
kalitekirkagac-sys <kalitekirkagac@gmail.com>
2026-09-16 21:58:52 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'sds-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(r =>
+      r || fetch(e.request).then(res => {
+        const cl = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, cl));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
